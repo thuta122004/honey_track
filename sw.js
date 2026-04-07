807ee07a872ee03cb61d4e3c0b315d1eb4dc05f2
Thu Ta <thuta122004@gmail.com>
2026-04-07 22:14:11 +0630

    add web app feature

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'honeytrack-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
